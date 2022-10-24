bffba0f01a1aac67b77edf1e296975b54b23319c
Aurelia Lim <aureliadotlim@gmail.com>
2022-10-25 00:06:25 +0800

    vue components

---
+++ resources/js/Pages/ShortUrl/List.vue
@@ -0,0 +1,62 @@
+<script setup>
+import Layout from '@/Shared/Layout.vue'
+import Icon from '@/Shared/Icon.vue'
+import Pagination from '@/Shared/Pagination.vue'
+import { Head, Link} from '@inertiajs/inertia-vue3';
+const props = defineProps(['short_urls']);
+</script>
+
+<template>
+    <Layout>
+      <div>
+        <Head title="Short Urls" />
+    <h1 class="mb-8 text-3xl font-bold">Short Urls</h1>
+    <div class="flex items-center justify-between mb-6">
+      <Link class="btn-indigo" href="/short_url">
+        <span>Create</span>
+        <span class="hidden md:inline">&nbsp;Short Url</span>
+      </Link>
+    </div>
+    <div class="bg-white rounded-md shadow overflow-x-auto">
+      <table class="w-full whitespace-nowrap">
+        <thead>
+          <tr class="text-left font-bold">
+            <th class="pb-4 pt-6 px-6">ID</th>
+            <th class="pb-4 pt-6 px-6">Slug</th>
+            <th class="pb-4 pt-6 px-6" colspan="2">Destination URL</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          <tr v-for="short_url in short_urls.data" :key="short_url.id" class="hover:bg-gray-100 focus-within:bg-gray-100">
+            <td class="border-t">
+              <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/short_urls/${short_url.id}/edit`">
+                {{ short_url.id }}
+              </Link>
+            </td>
+            <td class="border-t">
+              <Link class="flex items-center px-6 py-4" :href="`/short_urls/${short_url.id}/edit`" tabindex="-1">
+                {{ short_url.slug}}
+              </Link>
+            </td>
+            <td class="border-t">
+              <Link class="flex items-center px-6 py-4" :href="`/short_urls/${short_url.id}/edit`" tabindex="-1">
+                {{ short_url.destination_url }}
+              </Link>
+            </td>
+            <td class="w-px border-t">
+              <Link class="flex items-center px-4" :href="`/short_urls/${short_url.id}/edit`" tabindex="-1">
+                <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400" />
+              </Link>
+            </td>
+          </tr>
+          <tr v-if="short_urls.data.length === 0">
+            <td class="px-6 py-4 border-t" colspan="4">No short_urls found.</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <pagination class="mt-6" :links="short_urls.links" />
+  </div>
+</Layout>
+</template>
---
+++ resources/js/Shared/MainMenu.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <div class="mb-4">
+        <Link class="group flex items-center py-3" href="/">
+          <icon name="dashboard" class="mr-2 w-4 h-4" :class="isUrl('/') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+          <div :class="isUrl('/') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Dashboard</div>
+        </Link>
+      </div>
+      <div class="mb-4">
+        <Link class="group flex items-center py-3" href="/short_urls">
+          <icon name="office" class="mr-2 w-4 h-4" :class="isUrl('/short_urls') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+          <div :class="isUrl('/short_urls') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Smol URLs</div>
+        </Link>
+      </div>
+      <div class="mb-4">
+        <Link class="group flex items-center py-3" href="/reports">
+          <icon name="printer" class="mr-2 w-4 h-4" :class="isUrl('reports') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+          <div :class="isUrl('reports') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Reports</div>
+        </Link>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  import { Link } from '@inertiajs/inertia-vue3'
+  import Icon from '@/Shared/Icon.vue'
+
+  export default {
+    components: {
+      Icon,
+      Link,
+    },
+    methods: {
+      isUrl(...urls) {
+        let currentUrl = this.$page.url.substr(1)
+        if (urls[0] === '') {
+          return currentUrl === ''
+        }
+        return urls.filter((url) => currentUrl.startsWith(url)).length
+      },
+    },
+  }
+  </script>
